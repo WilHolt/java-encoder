5b557e7693c4f76b0ea54baa3da8254747937367
José <joswilder46@gmail.com>
2019-10-29 17:13:29 -0300

    descomentando

--- src/com/wil/Main.java
+++ src/com/wil/Main.java
@@ -20,52 +20,52 @@ public class Main {
 
     public static void main(String[] args)  throws FileNotFoundException, IOException {
         heapQueue fila = new heapQueue(10);
-
-        String filename = "C:/Users/joswi/Documents/lola.txt";
-        Map<Character,Integer> repetitions = new HashMap<Character, Integer>();
-        try (BufferedReader buffer = new BufferedReader(new InputStreamReader(
-                new FileInputStream(filename), StandardCharsets.UTF_8));) {
-            String line;
-            line = buffer.readLine();
-            for(int i =0; i< line.length(); i++){
-                char aux = line.charAt(i);
-                Integer count = repetitions.get(aux);
-                if (count == null) {
-                    repetitions.put(aux,0);
-
-                }
-                repetitions.put(aux, repetitions.get(aux) + 1);
-            }
-
-        }
-        Set<Character> a = repetitions.keySet();
-        for (Character t : a) {
-            Pair item =new  Pair<>(t,repetitions.get(t));
-            Node node= new Node(item);
-            fila.insert(node);
-
-                System.out.println(t + " Ocurred " + repetitions.get(t) + " times");
-        }
-//        Pair item3 =new  Pair<>('p',1);
-//        Pair item5 =new  Pair<>('a',3);
-//        Pair item4 =new  Pair<>('z',1);
-//        Pair item1 =new  Pair<>('l',4);
-//        Pair item2 =new  Pair<>('o',3);
 //
+//        String filename = "C:/Users/joswi/Documents/lola.txt";
+//        Map<Character,Integer> repetitions = new HashMap<Character, Integer>();
+//        try (BufferedReader buffer = new BufferedReader(new InputStreamReader(
+//                new FileInputStream(filename), StandardCharsets.UTF_8));) {
+//            String line;
+//            line = buffer.readLine();
+//            for(int i =0; i< line.length(); i++){
+//                char aux = line.charAt(i);
+//                Integer count = repetitions.get(aux);
+//                if (count == null) {
+//                    repetitions.put(aux,0);
 //
+//                }
+//                repetitions.put(aux, repetitions.get(aux) + 1);
+//            }
 //
-//        Node t1 = new Node(item1);
-//        Node t2 = new Node(item2);
-//        Node t3 = new Node(item3);
-//        Node t4 = new Node(item4);
-//        Node t5 = new Node(item5);
+//        }
+//        Set<Character> a = repetitions.keySet();
+//        for (Character t : a) {
+//            Pair item =new  Pair<>(t,repetitions.get(t));
+//            Node node= new Node(item);
+//            fila.insert(node);
 //
-//        //act
-//        fila.insert(t5);
-//        fila.insert(t2);
-//        fila.insert(t3);
-//        fila.insert(t4);
-//        fila.insert(t1);
+//                System.out.println(t + " Ocurred " + repetitions.get(t) + " times");
+//        }
+        Pair item3 =new  Pair<>('p',1);
+        Pair item5 =new  Pair<>('a',3);
+        Pair item4 =new  Pair<>('z',1);
+        Pair item1 =new  Pair<>('l',4);
+        Pair item2 =new  Pair<>('o',3);
+
+
+
+        Node t1 = new Node(item1);
+        Node t2 = new Node(item2);
+        Node t3 = new Node(item3);
+        Node t4 = new Node(item4);
+        Node t5 = new Node(item5);
+
+        //act
+        fila.insert(t5);
+        fila.insert(t2);
+        fila.insert(t3);
+        fila.insert(t4);
+        fila.insert(t1);
 
 
 
